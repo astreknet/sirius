258f84d64a9d31dc3196638d7b6f73a8397a3ba9
hugo <hugo@astrek.net>
2023-03-20 11:38:17 +0200

    validating html actio

--- views/header.php
+++ views/header.php
@@ -35,7 +35,7 @@
     }
     else {
         echo '
-            <form id="login" action="" method="POST">
+            <form id="login"  method="POST">
                 <input type="email" id="username" name="username" required maxlength="45" placeholder="username" value="'.value('username').'" autocomplete="username">
                 <input type="password" id="lpassword" name="lpassword" required maxlength="45" placeholder="password" autocomplete="current-password">
                 <input class="button" type="submit" value="log in">
