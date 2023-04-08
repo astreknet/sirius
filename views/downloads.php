ab47b5819d0eccb9ff684eb80df8774afca1f21f
hugo <hugo@astrek.net>
2023-04-08 10:02:43 +0300

    download reports

--- views/downloads.php
+++ views/downloads.php
@@ -12,9 +12,48 @@ if(isset($_SESSION['vcards'], $_SESSION['validated']) && $_SESSION['validated'])
             fwrite($out, "TEL;type=CELL:".$v['tel']."\n");
             fwrite($out, "END:VCARD\n");
     }
+    unset($_SESSION['vcards']);
     fclose($out);
 header( 'Content-Type: text/vcf' );
 header( 'Content-Disposition: attachment;filename=guides.vcf');
 }
+
+if(isset($_SESSION['trip_report'], $_SESSION['validated']) && $_SESSION['validated']) {
+    $filename = 'trip_report';
+    $out = fopen('php://output', 'w');
+    foreach ($_SESSION['trip_report'] as $t){
+        fputcsv($out, $t);
+    }
+    fclose($out);
+    unset($_SESSION['trip_report']);
+    header( 'Content-Type: text/csv' );
+    header( 'Content-Disposition: attachment;filename='.$filename);
+}
+
+if(isset($_SESSION['accident_report'], $_SESSION['validated']) && $_SESSION['validated']) {
+    $filename = 'accident_report';
+    $out = fopen('php://output', 'w');
+    foreach ($_SESSION['accident_report'] as $t){
+        fputcsv($out, $t);
+    }
+    fclose($out);
+    unset($_SESSION['accident_report']);
+    header( 'Content-Type: text/csv' );
+    header( 'Content-Disposition: attachment;filename='.$filename);
+}
+
+if(isset($_SESSION['nearmiss_report'], $_SESSION['validated']) && $_SESSION['validated']) {
+    $filename = 'nearmiss_report';
+    $out = fopen('php://output', 'w');
+    foreach ($_SESSION['nearmiss_report'] as $t){
+        fputcsv($out, $t);
+    }
+    fclose($out);
+    unset($_SESSION['nearmiss_report']);
+    header( 'Content-Type: text/csv' );
+    header( 'Content-Disposition: attachment;filename='.$filename);
+}
+
+
 
 ?>
