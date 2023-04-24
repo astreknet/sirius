e1036966c76bc0097fd2556a6e51676d3baab960
hugo <hugo@astrek.net>
2023-04-24 11:31:54 +0300

    login forgot pass align

--- views/login.php
+++ views/login.php
@@ -13,8 +13,10 @@ if (isset($_GET['user_lock'])){
         <h3>restart pasword</h3>
         <form id="restart_password" method="POST">
             <input type="email" id="username" name="username" required maxlength="45" placeholder="mail" '.value('username').' autocomplete="username"><br>
-            <input class="button" type="submit" value="restart password">
-            <div class="button"><a href="index.php">log in</a></div>
+            <div class="rack">
+                <div class="pre_button"><a href="index.php">log in</a></div>
+                <input class="button" type="submit" value="restart password">
+            </div>
         </form>
     ';
 
@@ -26,8 +28,10 @@ else{
         <form id="login" method="POST">
             <input type="email" id="username" name="username" required maxlength="45" placeholder="mail" '.value('username').' autocomplete="username"><br>
             <input type="password" id="lpassword" name="lpassword" required maxlength="45" placeholder="password" autocomplete="current-password"><br>
-            <input class="button" type="submit" value="log in">
-            <div class="button"><a href="?user_lock">forgot pass</a></div>
+            <div class="rack">
+                <div class="pre_button"><a href="?user_lock">forgot pass</a></div>
+                <input class="button" type="submit" value="log in">
+            </div>
         </form>
     ';  
 }
