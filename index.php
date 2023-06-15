6d8ddab689a1cf3e17889ae180c151a17752b471
hugo <hugo@astrek.net>
2023-06-15 11:09:02 +0200

    fixing deleting field on update

--- index.php
+++ index.php
@@ -292,7 +292,7 @@ class Guide extends User{
 
     public function updateTrip($table, $tableId, $inputs, $checks, $pdo){
         foreach ($inputs as $in) {
-            (!isset($post[$in]) && empty($_POST[$in]) ?: updateTableItemWhere($table, $in, $_POST[$in], 'id', $tableId, $pdo));
+            (!isset($_POST[$in]) && empty($_POST[$in]) ?: updateTableItemWhere($table, $in, $_POST[$in], 'id', $tableId, $pdo));
         }
         foreach($checks as $c) {
             (isset($_POST[$c]) ? updateTableItemWhere($table, $c, 1, 'id', $tableId, $pdo) : updateTableItemWhere($table, $c, 0, 'id', $tableId, $pdo));
