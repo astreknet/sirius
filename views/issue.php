606ccf17127da8ff3ce675e573b0aefaa3764f23
hugo <hugo@astrek.net>
2023-06-15 11:01:11 +0200

    modify issues

--- views/issue.php
+++ views/issue.php
@@ -1,18 +1,18 @@
 <section id="issues">
     <h3>Work Issues</h3>
 <?php
+$me = new Guide($_SESSION['usermail'], $pdo);
 $mytime = new DateTime('NOW');
 $issues = selectAllFromWhere('issue', 'user_id', $me->id, $pdo);
 $form_action = '?issues';
 $submit = "add issue";
+$first_aid = $hospital_visit = '';
 if (isset($_POST['datetime'], $_POST['place'], $_POST['description'])) {
-    $issueId = insertInto('issue', 'user_id', $me->id, $pdo);
-    updateTableItemWhere('issue', 'datetime', $_POST['datetime'], 'id', $issueId['id'], $pdo);
-    updateTableItemWhere('issue', 'place', $_POST['place'], 'id', $issueId['id'], $pdo);
-    updateTableItemWhere('issue', 'description', $_POST['description'], 'id', $issueId['id'], $pdo);
-    (!isset($_POST['injury']) || empty($_POST['injury']) ?: updateTableItemWhere('issue', 'injury', $_POST['injury'], 'id', $issueId['id'], $pdo));
-    (!isset($_POST['first_aid']) ?: updateTableItemWhere('issue', 'first_aid', 1, 'id', $issueId['id'], $pdo));
-    (!isset($_POST['hospital_visit']) ?: updateTableItemWhere('issue', 'hospital_visit', 1, 'id', $issueId['id'], $pdo));
+    $issueId = (isset($_GET['id']) ? $_GET['id'] : insertInto('issue', 'user_id', $me->id, $pdo));
+    $issueId = (is_array($issueId) ? $issueId['id'] : $issueId);
+    $inputs = array('datetime', 'place', 'description', 'injury');
+    $checks = array('first_aid', 'hospital_visit');
+    $me->updateTrip('issue', $issueId, $inputs, $checks, $pdo);
     header( "refresh:0;url=./?issues" );
 }
 
@@ -23,20 +23,26 @@ if (isset($_GET['id'])) {
         }
         $form_action = '?issues&id='.$_GET['id'];
         $submit = "update issue";
-        $first_aid = ($_SESSION['first_aid'] ? 'checked' : '');
         $hospital_visit = ($_SESSION['hospital_visit'] ? 'checked' : '');
     }
 
 echo '  
         <form action="'.$form_action.'" method="POST">
-            <input type="datetime-local" id="datetime" name="datetime" required max="'.$mytime->format("Y-m-d H:i").'">
-            <input type="text" id="place" name="place" required maxlength="150" placeholder="place">
-            <textarea id="description" name="description" required maxlength="270" placeholder="description"></textarea><br>
-            <textarea id="injury" name="injury" maxlength="270" placeholder="injury"></textarea><br>
-            <input type="checkbox" id="first_aid" name="first_aid"> first aid<br>
-            <input type="checkbox" id="hospital_visit" name="hospital visit"> hospital visit<br>
+            <input type="datetime-local" id="datetime" name="datetime" required max="'.$mytime->format("Y-m-d H:i").'" value="'.value('datetime').'">
+            <input type="text" id="place" name="place" required maxlength="150" placeholder="place" value="'.value('place').'">
+            <textarea id="description" name="description" required maxlength="270" placeholder="description">'.value('description').'</textarea><br>
+            <textarea id="injury" name="injury" maxlength="270" placeholder="injury">'.value('injury').'</textarea><br>
+            <input type="checkbox" id="first_aid" name="first_aid" '.$first_aid.'> first aid<br>
+            <input type="checkbox" id="hospital_visit" name="hospital visit" '.$hospital_visit.'> hospital visit<br>
             <input type="submit" class="button" value="'.$submit.'"><br>
         </form> ';
+
+if (isset($iss)){
+        foreach ($iss[0] as $k => $v) {
+            unset($_SESSION[$k]);
+        }
+    }
+
 if (count($issues) > 0) {
     echo '<ol>';
     foreach ($issues as $i){
